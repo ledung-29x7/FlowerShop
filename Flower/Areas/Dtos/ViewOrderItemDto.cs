4f991e5f434b2a3479310a050d5b5832acc2d90f
ANON-n <thodibar2201@gmail.com>
2024-11-24 23:41:24 +0700

    litlle change

--- Flower/Areas/Dtos/ViewOrderItemDto.cs
+++ Flower/Areas/Dtos/ViewOrderItemDto.cs
@@ -7,19 +7,19 @@ namespace Flower.Areas.Dtos
 {
     public class ViewOrderItemDto
     {
-        // ID của hoa
+        
         public int FlowerId { get; set; }
 
-        // Tên của hoa
+        
         public string FlowerName { get; set; }
 
-        // Số lượng
+        
         public int Quantity { get; set; }
 
-        // Đơn giá
+        
         public decimal UnitPrice { get; set; }
 
-        // Tổng giá trị (tính toán động, không cần lưu trữ)
+        
         public decimal TotalPrice => Quantity * UnitPrice;
     }
 }
